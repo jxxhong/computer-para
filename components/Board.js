624ec12d7f18380b7bba19f1b3ee1ccb55c1f418
Jaehong Ahn <100734717+jxxhong@users.noreply.github.com>
2025-01-11 23:05:41 +0900

    Feat : Each Item List

--- components/Board.js
+++ components/Board.js
@@ -1,6 +1,33 @@
-import { Modal, View, Text, Pressable, StyleSheet } from 'react-native';
+import {
+	Modal,
+	View,
+	Text,
+	Pressable,
+	StyleSheet,
+	FlatList,
+} from 'react-native';
 
 export default function Board(props) {
+	function itemPress(name) {
+		console.log(name);
+	}
+
+	const Item = ({ item }) => {
+		if (item.id === props.id)
+			return item.list.map((a) => {
+				return (
+					<Pressable style={styles.item} onPress={() => itemPress(a.itemName)}>
+						<View>
+							<Text>{a.itemName}</Text>
+							<Text>
+								{a.price1} / {a.price2}
+							</Text>
+						</View>
+					</Pressable>
+				);
+			});
+	};
+
 	return (
 		<Modal visible={props.visible} animationType="slide">
 			<View style={styles.navbar}>
@@ -10,6 +37,16 @@ export default function Board(props) {
 				<View style={styles.textView}>
 					<Text style={styles.mainText}>{props.id}</Text>
 				</View>
+			</View>
+			<View>
+				<FlatList
+					style={styles.itemList}
+					data={itemList}
+					renderItem={Item}
+					keyExtractor={(item, index) => {
+						return Math.random().toString();
+					}}
+				/>
 			</View>
 		</Modal>
 	);
@@ -51,4 +88,64 @@ const styles = StyleSheet.create({
 		paddingTop: 5,
 		paddingLeft: 10,
 	},
+	itemList: {
+		width: '100%',
+	},
+	item: {
+		fontSize: 30,
+		backgroundColor: '#dbdbdb',
+		borderRadius: 6,
+		margin: 10,
+		height: 80,
+	},
+	itemText1: {},
+	itemText2: {},
 });
+
+const itemList = [
+	{
+		id: 'gpu',
+		list: [
+			{ star: true, itemName: 'rtx 4080', price1: 900000, price2: 1200000 },
+		],
+	},
+	{
+		id: 'cpu',
+		list: [
+			{ star: false, itemName: 'i7-13900', price1: 800000, price2: 1200000 },
+			{ star: false, itemName: 'i7-13900', price1: 800000, price2: 1200000 },
+		],
+	},
+	{
+		id: 'ram',
+		list: [
+			{
+				star: false,
+				itemName: 'samsung ddr4 16GB',
+				price1: 150000,
+				price2: 1200000,
+			},
+		],
+	},
+	{
+		id: 'ssd',
+		list: [
+			{
+				star: true,
+				itemName: 'samsung m.2 1TB',
+				price1: 320000,
+				price2: 1200000,
+			},
+		],
+	},
+	{
+		id: 'cooler',
+		list: [{ star: false, itemName: 'cooler', price1: 30000, price2: 1200000 }],
+	},
+	{
+		id: 'mainboard',
+		list: [
+			{ star: true, itemName: 'msi b450m', price1: 70000, price2: 1200000 },
+		],
+	},
+];
